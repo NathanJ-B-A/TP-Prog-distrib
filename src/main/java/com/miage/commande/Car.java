953919a989d9bb9fdab2f6c0016c3603d40aea8b
Beeshop <jbanathan@gmail.com>
2024-10-25 10:58:20 +0200

    newcarservice

--- src/main/java/com/miage/commande/Car.java
+++ src/main/java/com/miage/commande/Car.java
@@ -1,38 +1,83 @@
 package com.miage.commande;
 
+import java.time.LocalDate;
+
 public class Car {
-    private String plateNumber;
     private String brand;
-    private float price;
+    private String plateNumber;
+    private int year;
+    private boolean available;
+    private LocalDate rentalStartDate;
+    private LocalDate rentalEndDate;
 
-    public Car(String plateNumber, String brand, float price) {
-
-        this.plateNumber = plateNumber;
+    // Constructeur
+    public Car(String brand, String plateNumber, int year, boolean available) {
         this.brand = brand;
-        this.price = price;
+        this.plateNumber = plateNumber;
+        this.year = year;
+        this.available = available;
+    }
+
+    // Getters et Setters pour les nouveaux champs
+    public LocalDate getRentalStartDate() {
+        return rentalStartDate;
+    }
+
+    public void setRentalStartDate(LocalDate rentalStartDate) {
+        this.rentalStartDate = rentalStartDate;
+    }
+
+    public LocalDate getRentalEndDate() {
+        return rentalEndDate;
+    }
+
+    public void setRentalEndDate(LocalDate rentalEndDate) {
+        this.rentalEndDate = rentalEndDate;
+    }
+
+    // Getters et Setters existants
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
     }
 
     public String getPlateNumber() {
         return plateNumber;
-    };
+    }
 
-    public void setPlateNumber(String newPlateNumber) {
-        this.plateNumber = newPlateNumber;
-    };
+    public void setPlateNumber(String plateNumber) {
+        this.plateNumber = plateNumber;
+    }
 
-    public String getBrand() {
-        return brand;
-    };
+    public int getYear() {
+        return year;
+    }
 
-    public void setBrand(String newBrand) {
-        this.brand = newBrand;
-    };
+    public void setYear(int year) {
+        this.year = year;
+    }
 
-    public float getPrice() {
-        return price;
-    };
+    public boolean isAvailable() {
+        return available;
+    }
 
-    public void setPrice(float newPrice) {
-        this.price = newPrice;
-    };
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", plateNumber='" + plateNumber + '\'' +
+                ", year=" + year +
+                ", available=" + available +
+                ", rentalStartDate=" + rentalStartDate +
+                ", rentalEndDate=" + rentalEndDate +
+                '}';
+    }
 }
